9594883a11cca4e6914be6046b8ef93d4194fea3
JamesOverclock <wirayut.temsao@gmail.com>
2019-01-04 10:36:29 +0700

    Initial Commit

---
+++ TestAutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestAutoLayout
+//
+//  Created by JamesOverclock on 20/6/2561 BE.
+//  Copyright © 2561 l3ios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+}
+
